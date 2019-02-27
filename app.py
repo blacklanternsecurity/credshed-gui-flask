a0440803119b93efba3512d7c5ee493f2212a75b
Joel Moore <joel.moore@blacklanternsecurity.com>
2019-02-27 16:39:22 -0500

    modularity, POST to /search

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # create the application object
 app = Flask(__name__)
@@ -11,6 +11,13 @@ app = Flask(__name__)
 def home():
     return render_template('pages/home.html')
 
+
+# use decorators to link the function to a url
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == 'POST':
+    	return render_template('pages/search_results.html', query=request.form['query'])
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
